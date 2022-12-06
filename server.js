6027072fbd1381747d942ca1a952290eb3c463a5
natolay <ndixon@seattleu.edu>
2022-12-06 22:47:04 +0100

    used get route to fetch the https api and response json

--- server.js
+++ server.js
@@ -10,3 +10,12 @@ const PORT = 3000;
 server.set('views', './src/views');
 server.set('view engine', 'ejs');
 
+server.get('/', (req, res) => {
+    
+    fetch('https://api.giphy.com/v1/gifs/trending?limit=10&api_key=MkJI3oqOdaTmFPwfvq1jyod3YyO6E1Sq')
+        
+        .then(response => response.json())
+        // .then(data => console.log(data.data))
+        .then(data => res.render('index', {data: data.data}))
+        .catch((error) => console.error('Error from API'))
+})
